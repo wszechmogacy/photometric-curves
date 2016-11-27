ff1ccacde9f4f8272696b5decd3c0ad09833bc76
Tomasz Boski <boski.tomasz@gmail.com>
2016-11-27 15:05:44 +0100

    Full inregral calculation

--- luminous-flux-calculator.h
+++ luminous-flux-calculator.h
@@ -14,6 +14,8 @@ public:
 private:
     const double sphere_radius;
     double calculate_meridian_chunck_length(int meridian_points_count);
+    double sum_parallels_values(double lattitude, std::vector<Point> &data);
+    std::vector<double> create_paralles_list(int meridian_points_count);
 };
 
 #endif // LUMINOUSFLUXCALCULATOR_H
--- luminousfluxcalculator.cpp
+++ luminousfluxcalculator.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <numeric>
 #include <QDebug>
 #include <vector>
 
@@ -18,47 +19,51 @@ double LuminousFluxCalculator::calculate_meridian_chunck_length(int meridian_poi
 
 double LuminousFluxCalculator::operator() (std::vector<Point> &data, int meridian_points_count, int parallel_points_count)
 {
-    const int slices = 18;
+    auto parallels = create_paralles_list(meridian_points_count);
+
+    double area = 0.0;
+    double flux = 0.0;
+    for (double upper_lattidtude : parallels) {
+        double lower_lattidtude = upper_lattidtude - 90.0 / meridian_points_count;
+
+        const double upper_circumference = 2 * ConstNumbers::pi * this->sphere_radius * std::cos(upper_lattidtude * 2 * ConstNumbers::pi / 360.0);
+        const double lower_circumference = 2 * ConstNumbers::pi * this->sphere_radius * std::cos(lower_lattidtude * 2 * ConstNumbers::pi / 360.0);
+        area = (upper_circumference + lower_circumference) / 2 * calculate_meridian_chunck_length(meridian_points_count);
+
+        for (auto arg : parallels) qDebug() << QString::number(arg);
+
+        double upper_sum = sum_parallels_values(upper_lattidtude, data);
+        double lower_sum = sum_parallels_values(lower_lattidtude, data);
+
+        flux += (upper_sum + lower_sum) * area / 2;
+    }
+
+    return flux;
+}
+
+double LuminousFluxCalculator::sum_parallels_values(double lattidtude, std::vector<Point> &data)
+{
+    std::vector<Point> points_on_layer;
+    for (auto arg : data) {
+        if (arg.lat_angle_deg == lattidtude) points_on_layer.push_back(arg);
+    }
+    double sum = std::accumulate(points_on_layer.begin(), points_on_layer.end(), 0.0,
+                                 [](double val, const Point &point){ return val + point.radial;}
+    );
+    qDebug() << "sum=" << QString::number(sum) << " for lat=" << QString::number(lattidtude);
+    return sum;
+}
+
+std::vector<double> LuminousFluxCalculator::create_paralles_list(int meridian_points_count)
+{
+    const int slices = meridian_points_count;
     std::vector<double> parallels(slices);
-    std::iota(parallels.begin(), parallels.end(), 0);
+    std::iota(parallels.begin(), parallels.end(), 0.0);
 
     std::transform(parallels.begin(),
                    parallels.end(),
                    parallels.begin(),
-                   [](double val) { return (90.0 - (90.0 / slices) * val);}
-
+                   [&slices](double val) { return (90.0 - (90.0 / slices) * val);}
     );
-
-
-    qDebug() << "===========================" ;
-    for (auto arg : parallels) qDebug() << QString::number(arg);
-
-    double area = 0.0;
-    for (double upper_lattidtude : parallels) {
-        double lower_lattidtude = upper_lattidtude - 90.0 / slices;
-
-        const double upper_circumference = 2 * ConstNumbers::pi * this->sphere_radius * std::cos(upper_lattidtude * 2 * ConstNumbers::pi / 360.0);
-        const double lower_circumference = 2 * ConstNumbers::pi * this->sphere_radius * std::cos(lower_lattidtude * 2 * ConstNumbers::pi / 360.0);
-        area += (upper_circumference + lower_circumference) / 2 * calculate_meridian_chunck_length(slices);
-    }
-    qDebug() << "area=" << QString::number(area);
-
-    //qDebug() << "circ=" << QString::number(upper_circumference) << QString::number(lower_circumference);
-
-
-
-    /*
-    std::vector<Point> top_layer;
-    for (auto arg : data) {
-        if (arg.lat_angle_deg == upper_lattidtude) top_layer.push_back(arg);
-    }
-
-
-
-    for (auto arg : top_layer)
-        qDebug() << arg.lat_angle_deg << " " << arg.lon_angle_deg;
-
-*/
-
-    return area;
+    return parallels;
 }
