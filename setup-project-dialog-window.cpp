6889d5e8cc2849134a2c609f1c566c351062e9d1
wilq <tomasz.wilk@vp.pl>
2017-01-09 22:51:35 +0100

    Localize setup-project

--- setup-project-dialog-window.cpp
+++ setup-project-dialog-window.cpp
@@ -11,6 +11,24 @@ SetupProjectDialogWindow::SetupProjectDialogWindow(ProjectSettings *proj, QWidge
     project_settings_(proj)
 {
     ui_->setupUi(this);
+
+    ui_->classesInformationGroupBox->setTitle(tr("Classes information"));
+    ui_->studentNameLabel->setText(tr("Students' names"));
+    ui_->studentNameField->setText("Tomasz Wilk");
+    ui_->classesDateLabel->setText(tr("Date of classes"));
+    ui_->classesDateField->setText("2017-01-28");
+
+    ui_->lightSourceGroupBox->setTitle(tr("Measured light source"));
+    ui_->lightSourceLabel->setText(tr("Source name"));
+    ui_->lightSourceField->setText("Oprawa 1");
+
+    ui_->enviromentSettingsGroupBox->setTitle(tr("Measurement settings"));
+    ui_->stepInParallelLayout_2->setText(tr("Step in parallel direction"));
+    ui_->stepInMeridianLabel->setText(tr("Step in meridian direction"));
+    ui_->unitsLabel->setText(tr("Units"));
+    ui_->radiusLabel->setText("Radius of measured sphere");
+    ui_->radiusField->setText("0.200");
+
     const double luminous_flux_to_watts_ratio = 683.0;
     units_items_ = {
         qMakePair(QString("W/m^2"), 1.0),
@@ -41,7 +59,6 @@ void SetupProjectDialogWindow::on_startProjectButton_clicked()
 {
     project_settings_->student_name_ = ui_->studentNameField->text();
     project_settings_->class_date_ = ui_->classesDateField->text();
-    project_settings_->student_group_ = ui_->studentGroupField->text();
     project_settings_->source_name_ = ui_->lightSourceField->text();
     project_settings_->step_in_parallel_dir_ = ui_->stepParallelComboBox->currentText().toUInt();
     project_settings_->step_in_meridian_dir_ = ui_->stepInMeridianComboBox->currentText().toUInt();
