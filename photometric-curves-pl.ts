bd2bde1cf1d4c71a492aaa22c4cefe22200fe45a
wilq <tomasz.wilk@vp.pl>
2017-01-10 22:48:03 +0100

    Add translation file

---
+++ photometric-curves-pl.ts
@@ -0,0 +1,293 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL" sourcelanguage="en_GB">
+<context>
+    <name>IntroductionDialogWindow</name>
+    <message>
+        <location filename="introduction-dialog-window.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished">Krzywe Fotometryczne</translation>
+    </message>
+    <message>
+        <location filename="introduction-dialog-window.cpp" line="14"/>
+        <source>Photometric Curves</source>
+        <translation type="unfinished">Krzywe Fotometryczne</translation>
+    </message>
+    <message>
+        <location filename="introduction-dialog-window.cpp" line="15"/>
+        <source>Create Project</source>
+        <translation type="unfinished">Nowy Projekt</translation>
+    </message>
+    <message>
+        <location filename="introduction-dialog-window.cpp" line="16"/>
+        <source>Open Project</source>
+        <translation type="unfinished">Otwórz Projekt</translation>
+    </message>
+</context>
+<context>
+    <name>LuminousFluxWindow</name>
+    <message>
+        <location filename="luminous-flux-window.ui" line="17"/>
+        <source>Lumnious Flux</source>
+        <translation type="unfinished">Całkowity strumień świetlny</translation>
+    </message>
+    <message>
+        <location filename="luminous-flux-window.cpp" line="11"/>
+        <source>Close</source>
+        <translation type="unfinished">Zamknij</translation>
+    </message>
+    <message>
+        <location filename="luminous-flux-window.cpp" line="13"/>
+        <source>Luminous Flux = %1 lm</source>
+        <translation type="unfinished">Strumień świetlny= %1 lm</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">Krzywe Fotometryczne</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="50"/>
+        <source>Photometric Curves</source>
+        <translation type="unfinished">Krzywe Fotometryczne</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="52"/>
+        <source>Luminous Flux</source>
+        <translation type="unfinished">Strumień Świetlny</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="53"/>
+        <source>Photometric Curve</source>
+        <translation type="unfinished">Krzywa Fotometryczna</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="54"/>
+        <source>Read data</source>
+        <translation type="unfinished">Importuj Dane</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="55"/>
+        <source>Save to file</source>
+        <translation type="unfinished">Eksportuj do pliku</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="56"/>
+        <source>Section</source>
+        <translation type="unfinished">Przekrój</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="57"/>
+        <source>Photometric solid</source>
+        <translation type="unfinished">Bryła fotometryczna</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="207"/>
+        <location filename="main-window.cpp" line="224"/>
+        <source>Open File</source>
+        <translation type="unfinished">Otwórz Plik</translation>
+    </message>
+    <message>
+        <location filename="main-window.cpp" line="207"/>
+        <location filename="main-window.cpp" line="224"/>
+        <source>CSV Files (*.csv)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PolarGraphWindow</name>
+    <message>
+        <location filename="polar-graph-window.cpp" line="81"/>
+        <source>Photometric curve</source>
+        <translation type="unfinished">Krzywa Fotometryczna</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="83"/>
+        <source>Photometric section</source>
+        <translation type="unfinished">Przekrój</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="85"/>
+        <source>Photometric graph</source>
+        <translation type="unfinished">Wykres</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="91"/>
+        <source>dots</source>
+        <translation type="unfinished">punkty</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="92"/>
+        <source>curve</source>
+        <translation type="unfinished">obrys</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="93"/>
+        <source>spline</source>
+        <translation type="unfinished">estymata</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="128"/>
+        <source>Graph curve type</source>
+        <translation type="unfinished">Typ linii</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="131"/>
+        <source>scatter</source>
+        <translation type="unfinished">punkty</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="132"/>
+        <source>line</source>
+        <translation type="unfinished">estymata</translation>
+    </message>
+    <message>
+        <location filename="polar-graph-window.cpp" line="133"/>
+        <source>smooth line</source>
+        <translation type="unfinished">obrys</translation>
+    </message>
+</context>
+<context>
+    <name>SetupProjectDialogWindow</name>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished">Ustawienia Projektu</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="26"/>
+        <source>Classes Information</source>
+        <translation type="unfinished">Zajęcia</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="36"/>
+        <source>Students&apos; Names</source>
+        <translation type="unfinished">Nazwiska studentów</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="43"/>
+        <source>Tomasz Wilk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="56"/>
+        <location filename="setup-project-dialog-window.cpp" line="18"/>
+        <source>Date of classes</source>
+        <translation type="unfinished">Data</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="63"/>
+        <source>13. December 2016</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="87"/>
+        <source>Light Source Information</source>
+        <translation type="unfinished">Informacje o oprawie</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="102"/>
+        <source>Source Name</source>
+        <translation type="unfinished">Nazwa źródła</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="109"/>
+        <source>Oprawa 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="126"/>
+        <source>Environment Settings:</source>
+        <translation type="unfinished">Ustawienia środowiska:</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="143"/>
+        <location filename="setup-project-dialog-window.cpp" line="26"/>
+        <source>Step in parallel direction</source>
+        <translation type="unfinished">Krok na południku</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="161"/>
+        <location filename="setup-project-dialog-window.cpp" line="27"/>
+        <source>Step in meridian direction</source>
+        <translation type="unfinished">Krok na równoleżniku</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="175"/>
+        <location filename="setup-project-dialog-window.cpp" line="28"/>
+        <source>Units</source>
+        <translation type="unfinished">Jednostki</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="189"/>
+        <source>Radius of measured sphere</source>
+        <translation type="unfinished">Promień półsfery</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="196"/>
+        <source>0.200</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.ui" line="215"/>
+        <source>Start Project</source>
+        <translation type="unfinished">Zacznij Projekt</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.cpp" line="14"/>
+        <source>Project Settings</source>
+        <translation type="unfinished">Ustawienia Projektu</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.cpp" line="15"/>
+        <source>Classes information</source>
+        <translation type="unfinished">Zajęcia</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.cpp" line="16"/>
+        <source>Students&apos; names</source>
+        <translation type="unfinished">Nazwiska studenów</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.cpp" line="21"/>
+        <source>Measured light source</source>
+        <translation type="unfinished">Mierzone źródło światła</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.cpp" line="22"/>
+        <source>Source name</source>
+        <translation type="unfinished">Źródło światła</translation>
+    </message>
+    <message>
+        <location filename="setup-project-dialog-window.cpp" line="25"/>
+        <source>Measurement settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceWindow</name>
+    <message>
+        <location filename="surface-window.cpp" line="44"/>
+        <source>Photometric solid</source>
+        <translation type="unfinished">Bryła fotometryczna</translation>
+    </message>
+    <message>
+        <location filename="surface-window.cpp" line="62"/>
+        <source>Convert to PDF</source>
+        <translation type="unfinished">Eksportuj do PDFa</translation>
+    </message>
+    <message>
+        <location filename="surface-window.cpp" line="65"/>
+        <source>Rotate horizontally</source>
+        <translation type="unfinished">Obróć poziomo</translation>
+    </message>
+    <message>
+        <location filename="surface-window.cpp" line="67"/>
+        <source>Rotate vertically</source>
+        <translation type="unfinished">Obróć pionowo</translation>
+    </message>
+</context>
+</TS>
