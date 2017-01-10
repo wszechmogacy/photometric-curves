1074d588b4819d477dcdd68b8c4eaa16589f477a
wilq <tomasz.wilk@vp.pl>
2017-01-10 22:47:43 +0100

    launch translator

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <QApplication>
+#include <QTranslator>
+#include <QDebug>
 
 #include <main-window.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    translator.load("/home/tomasz/devel/photometric-curves/photometric-curves-pl.qm");
+    a.installTranslator(&translator);
 
     MainWindow *w = new MainWindow();
     w->show();
